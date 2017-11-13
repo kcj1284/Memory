d79ff7f04ea4d97490cdf84e5fffbb1ed8616631
kim gang min <family0238@naver.com>
2017-11-13 16:57:23 +0900

    2017.11.13 16:57 Modified jdbc jar path

--- window1/src/window1/DB.java
+++ window1/src/window1/DB.java
@@ -9,10 +9,10 @@ public class DB {
 	private static String DB_ID = "memory";
 	private static String DB_PASSWORD = "0238";
 	private static String DB_NAME = "memory";
-	private static String DB_IP = "";
+	private static String DB_IP = "localhost";
 	private static int DB_PORT = 3306;
 
-	private static String DB_Connect = "jdbc:mysql:" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
+	private static String DB_Connect = "jdbc:mysql://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
 
 	Connection conn;
 	Statement stmt;
@@ -25,23 +25,20 @@ public class DB {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(DB_Connect, DB_ID, DB_PASSWORD);
-
+			stmt = conn.createStatement();
 		} catch (ClassNotFoundException e) {
 			System.err.println("Error : DB Connect - Class Not Found");
 		} catch (SQLException e) {
 			System.err.println("Error : DB Connect - Driver Manager");
 		}
-
 	}
 
 	protected void closeDB() {
 		try {
 			if (conn != null)
 				conn.close();
-
 		} catch(SQLException e)	{
 			System.err.println("Error : DB Close");
 		}
 	}
-
 }
