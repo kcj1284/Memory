27a7e339175595f95a32497111cbe242091e45fc
kim chan jung <kcj1284@gmail.com>
2017-11-14 20:47:57 +0900

    11.14.20.47

--- window1/src/window1/DB.java
+++ window1/src/window1/DB.java
@@ -2,56 +2,84 @@ package window1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DB {
-	private static String DB_ID = "memory";
-	private static String DB_PASSWORD = "0238";
-	private static String DB_NAME = "memory";
-	private static String DB_IP = "localhost";
-	private static int DB_PORT = 3306;
+   private static String DB_ID = "memory";
+   private static String DB_PASSWORD = "0238";
+   private static String DB_NAME = "memory";
+   private static String DB_IP = "localhost";
+   private static int DB_PORT = 3306;
 
-	private static String DB_Connect = "jdbc:mysql://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
+   private static String DB_Connect = "jdbc:mysql://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
 
-	Connection conn;
-	Statement stmt;
+   Connection conn;
+   PreparedStatement stmt;
+   ResultSet rs;
 
-	public DB() {
-		connectDB();
-	}
+   public DB() {
+      connectDB();
+   }
 
-	protected void connectDB() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(DB_Connect, DB_ID, DB_PASSWORD);
-			stmt = conn.createStatement();
-		} catch (ClassNotFoundException e) {
-			System.err.println("Error : DB Connect - Class Not Found");
-		} catch (SQLException e) {
-			System.err.println("Error : DB Connect - Driver Manager");
-		}
-	}
+   protected void connectDB() {
+      try {
+         Class.forName("com.mysql.jdbc.Driver");
+         conn = DriverManager.getConnection(DB_Connect, DB_ID, DB_PASSWORD);
+      } catch (ClassNotFoundException e) {
+         System.err.println("Error : DB Connect - Class Not Found");
+      } catch (SQLException e) {
+         System.err.println("Error : DB Connect - Driver Manager");
+      }
+   }
 
-	protected boolean insertMember(String id, String passwd, String email, String name, int question, String answer) {
-		boolean isSuccess = false;
-		try {
-			String sql = "insert into member values(" + id + ", " + passwd + ", " + email + ", " + name + ", "
-					+ question + ", " + answer + ");";
+   public boolean getMember(){  // 유저 정보 갱신
+      boolean isSuccess = false;
+      try{
+         MemberInfo m;
+         Data.member_vector.removeAllElements();
 
-			isSuccess = stmt.execute(sql);
-		} catch (SQLException e) {
-			System.err.println("Error : Insert Member");
-		}
-		return isSuccess;
-	}
+         stmt = conn.prepareStatement("select * from member"); // 쿼리문 전송
+         rs = stmt.executeQuery();
 
-	protected void closeDB() {
-		try {
-			if (conn != null)
-				conn.close();
-		} catch (SQLException e) {
-			System.err.println("Error : DB Close");
-		}
-	}
+         while(rs.next()){ // result set이 더 있을 경우
+            m = new MemberInfo();
+            m.id = rs.getString("id");
+            m.pw = rs.getString("pw");
+            m.email = rs.getString("email");
+            m.name = rs.getString("name");
+            m.pwQuestion= Integer.parseInt(rs.getString("pwQuestion"));
+            m.pwAnswer = rs.getString("pwAnswer");
+
+            Data.member_vector.addElement(m); // 벡터에 유저 정보 추가
+         }
+         isSuccess = true;
+      } catch(SQLException e){
+
+      }
+      return isSuccess;
+   }
+
+   protected boolean insertMember(String id, String passwd, String email, String name, int question, String answer) {
+      boolean isSuccess = false;
+      try {
+         String sql = "insert into member values(\'" + id + "\', \'" + passwd + "\', \'" + email + "\', \'" + name + "\', "
+               + question + ", \'" + answer + "\');";
+
+         isSuccess = stmt.execute(sql);
+      } catch (SQLException e) {
+         System.err.println("Error : Insert Member");
+      }
+      return isSuccess;
+   }
+
+   protected void closeDB() {
+      try {
+         if (conn != null)
+            conn.close();
+      } catch (SQLException e) {
+         System.err.println("Error : DB Close");
+      }
+   }
 }
---
+++ window1/src/window1/Data.java
@@ -0,0 +1,8 @@
+package window1;
+
+import java.util.Vector;
+
+public class Data {
+   public static Vector<MemberInfo> member_vector = new Vector<MemberInfo>();
+   
+}
