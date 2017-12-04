dfcf829a7f26fc505267b58fea9e97f69a9f5bc1
colding97 <31755463+kcj1284@users.noreply.github.com>
2017-12-04 14:56:23 +0900

    12.04.14.56

--- window1/src/window1/Login.java
+++ window1/src/window1/Login.java
@@ -22,9 +22,9 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+
 /**
- * @author 김찬중
- * Login을 하는 Class
+ * @author 김찬중 Login을 하는 Class
  */
 public class Login extends JFrame {
 	DB db = new DB();
@@ -37,25 +37,24 @@ public class Login extends JFrame {
 	private JPasswordField tf_pw;
 
 	/**
-	 * @author 김찬중
-	 * Launch the application.
+	 * @author 김찬중 Launch the application.
 	 */
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Frame.frame_login
-					.setVisible(true);
+					Frame.frame_login.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
+
 	/**
 	 * Create the frame.
-	 */ 
+	 */
 	public Login() {
 		setTitle("Login");
 		setResizable(false);
@@ -63,13 +62,13 @@ public class Login extends JFrame {
 		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(50, 30, 1303, 829);
-  
+
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		tf_id = new JTextField();
 		tf_id.setFont(new Font("맑은 고딕", Font.PLAIN, 25));
 		tf_id.setBounds(170, 442, 250, 45);
@@ -85,7 +84,7 @@ public class Login extends JFrame {
 		JButton btn_join = new JButton("");
 		btn_join.setBorderPainted(false);
 		btn_join.setIcon(new ImageIcon("rsc\\icon\\btn_join.png"));
-		btn_join.setBounds(530, 442, 97, 97);
+		btn_join.setBounds(170, 551, 170, 50);
 		btn_join.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Frame.frame_join.reset();
@@ -93,36 +92,38 @@ public class Login extends JFrame {
 			}
 		});
 		contentPane.add(btn_join);
-		
+
 		JButton btn_find = new JButton("");
 		btn_find.setBorderPainted(false);
 		btn_find.setIcon(new ImageIcon("rsc\\icon\\btn_join.png"));
-		btn_find.setBounds(170, 551, 150, 51);
+		btn_find.setBounds(352, 551, 170, 50);
 		btn_find.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Frame.frame_findIDPW.setVisible(true);
 			}
 		});
 		contentPane.add(btn_find);
-		
+
 		tf_pw = new JPasswordField();
 		tf_pw.setFont(new Font("맑은 고딕", Font.PLAIN, 25));
 		tf_pw.setBounds(170, 494, 250, 45);
 		contentPane.add(tf_pw);
-		
-		class Listener implements ActionListener, KeyListener{
+
+		class Listener implements ActionListener, KeyListener {
 			public void actionPerformed(ActionEvent arg0) {
 				login();
 			}
 
-			public void keyPressed(KeyEvent e){
-				if(e.getKeyCode() == KeyEvent.VK_ENTER)
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER)
 					login();
 			}
 
-			public void keyReleased(KeyEvent e){}
+			public void keyReleased(KeyEvent e) {
+			}
 
-			public void keyTyped(KeyEvent e){}
+			public void keyTyped(KeyEvent e) {
+			}
 		}
 
 		Listener listener = new Listener();
@@ -130,7 +131,6 @@ public class Login extends JFrame {
 		btn_login.addActionListener(listener);
 		tf_id.addActionListener(listener);
 		tf_pw.addActionListener(listener);
-		
 
 		JLabel lb_background = new JLabel(new ImageIcon("rsc\\Login.jpg"));
 		lb_background.setBackground(Color.WHITE);
@@ -139,33 +139,33 @@ public class Login extends JFrame {
 	}
 
 	/**
-	 * @author 김찬중
-	 * 유저 정보를 불러와서 이에 해당하는 계정이 있으면 로그인을 하는 메소드
+	 * @author 김찬중 유저 정보를 불러와서 이에 해당하는 계정이 있으면 로그인을 하는 메소드
 	 */
-	
-	private void login(){
+
+	private void login() {
 		db.getMember();
 		id = tf_id.getText().toString();
 		pw = new String(tf_pw.getPassword());
-		if(id.length() == 0){
+		if (id.length() == 0) {
 			JOptionPane.showMessageDialog(Frame.frame_login, "아이디를 입력해주세요.", "오류", JOptionPane.ERROR_MESSAGE);
 			return;
 		}
-		if(pw.length() == 0){
+		if (pw.length() == 0) {
 			JOptionPane.showMessageDialog(Frame.frame_login, "비밀번호를 입력해주세요.", "오류", JOptionPane.ERROR_MESSAGE);
 			return;
 		}
 
-		if((index = db.isPWCorrect(id, pw)) != -1){
+		if ((index = db.isPWCorrect(id, pw)) != -1) {
 			Data.userIndex = index;
-			Frame.frame_home.setVisible(true); 
-			Frame.frame_popup.setVisible(true); 
+			Frame.frame_home.setVisible(true);
+			Frame.frame_popup.setVisible(true);
 			tf_id.setText("");
 			tf_pw.setText("");
-			Frame.frame_login.setVisible(false); 
+			Frame.frame_login.setVisible(false);
 			return;
-		} else{
-			JOptionPane.showMessageDialog(Frame.frame_login, "아이디 또는 비밀번호가 일치하지 않습니다.", "오류", JOptionPane.ERROR_MESSAGE);
+		} else {
+			JOptionPane.showMessageDialog(Frame.frame_login, "아이디 또는 비밀번호가 일치하지 않습니다.", "오류",
+					JOptionPane.ERROR_MESSAGE);
 		}
 	}
-} 
+}
